db92177df3d41ebf49b5bed1ceb7ce1bc837c2ec
h-chal <h@h-chal.com>
2024-09-26 13:00:34 +0100

    feat(gravity): add clack sound

--- gravity/sketch.js
+++ gravity/sketch.js
@@ -20,29 +20,40 @@ let maxHistory = 300;  // trail length
 const screenWidth = 800;
 const screenHeight = 800;
 
+function preload() {
+  clack = loadSound("clack.wav");
+}
+
 function setup() {
   frameRate(60)  // default
   createCanvas(screenWidth, screenHeight);
+
+  volE = createElement("h3", "Volume");
+  volE.style("color", "#0474cf");
+  volE.position(20, 5);
+  volSlider = createSlider(0, 0.4, 0.2, 0.05);
+  volSlider.style("width", "50px");
+  volSlider.position(20, 50)
   
   gravE = createElement("h3", "Gravitational Constant (default is 100):");
   gravE.style("color", "#0474cf");
-  gravE.position(20, 5);
+  gravE.position(20, 65);
   gravInput = createSlider(0, 1000, 100);
-  gravInput.position(20, 50);
+  gravInput.position(20, 110);
   gravInput.style("width", "250px")
   repulsive = createCheckbox("Repulsive", false)
-  repulsive.position(275, 50)
+  repulsive.position(275, 110)
   repulsive.style("color", "#0474cf")
   
   playSpeedE = createElement("h3", "Simulation Speed:")
-  playSpeedE.position(20, 70)
+  playSpeedE.position(20, 130)
   playSpeedE.style("color", "#0474cf");
   playSpeedInput = createSlider(0, 2, 1, 0.25)
-  playSpeedInput.position(20, 115)
+  playSpeedInput.position(20, 175)
   playSpeedInput.style("width", "100px");
   
   spawning = createCheckbox("Click to spawn balls", false)
-  spawning.position(20, 140)
+  spawning.position(20, 200)
   spawning.style("color", "#0474cf")
 }
 
@@ -109,20 +120,24 @@ class Ball {
     if (this.pos.x - this.radius <= 0) {
       this.pos.x = this.radius;
       this.vel.x = abs(this.vel.x);
+      clackSound = true;
     } 
     else if (this.pos.x >= screenWidth - this.radius) {
       this.pos.x = screenWidth - this.radius;
       this.vel.x = -1 * abs(this.vel.x);
+      clackSound = true;
     }
     
     // y:
     if (this.pos.y - this.radius <= 0) {
       this.pos.y = this.radius;
       this.vel.y = abs(this.vel.y);
+      clackSound = true;
     } 
     else if (this.pos.y >= screenHeight - this.radius) {
       this.pos.y = screenHeight - this.radius;
       this.vel.y = -1 * abs(this.vel.y);
+      clackSound = true;
     }
   }
   
@@ -160,6 +175,7 @@ class Ball {
       // don't resolve collision if it was resolved last frame
       return;
     }
+    clackSound = true;
     this.collidedLastFrame = true
     other.collidedLastFrame = true
     // followed https://spicyyoghurt.com/tutorials/html5-javascript-game-development/collision-detection-physics
@@ -226,15 +242,17 @@ for(var i=0; i<l; i++)
 
 function mousePressed() {
   // check if mouse is within rectangular areas to change sliders/checkboxes
-  let inGravArea = (mouseX > 15) && (mouseX < 360) &&
+  let inVolArea = (mouseX > 15) && (mouseX < 125) &&
     (mouseY > 50) && (mouseY < 70);
+  let inGravArea = (mouseX > 15) && (mouseX < 360) &&
+    (mouseY > 110) && (mouseY < 130);
   let inSpeedArea = (mouseX > 15) && (mouseX < 125) &&
-    (mouseY > 110) && (mouseY < 135);
+    (mouseY > 170) && (mouseY < 165);
   let inSpawnArea = (mouseX > 20) && (mouseX < 175) &&
-    (mouseY > 135) && (mouseY < 165);
+    (mouseY > 195) && (mouseY < 225);
   
   // don't spawn balls if changing sliders/checkboxes or if spawning is disabled
-  if (inGravArea || inSpeedArea || inSpawnArea || !spawning.checked()) {
+  if (inVolArea || inGravArea || inSpeedArea || inSpawnArea || !spawning.checked()) {
     return;
   }
   
@@ -248,16 +266,19 @@ function mousePressed() {
 }
 
 
-
+var clackSound = false;
 
 function draw() {  // mainloop
   background(220);
   
+  clackSound = false;
+
+  clack.setVolume(volSlider.value())
   fill("#0474cf")
   gravConst = gravInput.value()
-  text(gravConst, 25, 80)
+  text(gravConst, 25, 140)
   playSpeed = playSpeedInput.value()
-  text(str(playSpeed + "x"), 130, 130)
+  text(str(playSpeed + "x"), 130, 190)
   frameRate(60/playSpeed)
   fill(255, 255, 255)
   if (repulsive.checked()) {
@@ -278,5 +299,7 @@ function draw() {  // mainloop
     // collide is called within attract for efficiency
   }
   
-  
+  if (clackSound) {
+    clack.play()
+  }
 }
